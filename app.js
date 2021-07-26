fccea8724b865d4882e4784b57fa0b6802821b54
Akinyemi Taiwo k <karrim4real@gmail.com>
2021-07-26 14:08:00 +0800

    fix some error

--- app.js
+++ app.js
@@ -1,20 +1,44 @@
+// const express = require("express");
+// const rootDir = require("./util/path");
+// const path = require("path");
+// const MongoClient = require("mongodb").MongoClient;
+// const assert = require("assert");
+// const shopRoute = require("./routes/shop");
+// const bodyParser = require("body-parser");
+
 const express = require("express");
 const rootDir = require("./util/path");
 const path = require("path");
-const MongoClient = require("mongodb").MongoClient;
+const mongoose = require("mongoose");
 const assert = require("assert");
 const shopRoute = require("./routes/shop");
-const bodyParser = require("body-parser");
 
 const app = express();
 
+mongoose
+  .connect(
+    "mongodb+srv://karrim4real:7NTwDqkKvbdEliXY@cluster0.8tzc3.mongodb.net/devlab?retryWrites=true",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => console.log("Database connected successfully !!"))
+  .catch((err) => console.log(err));
+
+app.set("views", path.join(__dirname, "views"));
+
+// view engine setup
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "public")));
-require("./config/key");
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")));
+
 //Login middleware
 app.use(shopRoute);
 
@@ -28,4 +52,6 @@ app.use((req, res) => {
 
 const port = process.env.PORT || 8000;
 
-app.listen(port);
+app.listen(port, function () {
+  console.log(`app on port ${port}`);
+});
--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const shopRoutes = express.Router();
 const User = require("../model/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const jwtSecret = "Secet";
 // const bodyParser = require("body-parser");
 
 shopRoutes.get("/login", (req, res) => {
@@ -9,11 +12,53 @@ shopRoutes.get("/login", (req, res) => {
   });
 });
 
-shopRoutes.post("/login", (req, res, next) => {
-  const pass = req.body.password;
-  console.log(user, pass);
+shopRoutes.post("/login", async (req, res, next) => {
+  console.log(req.body);
+  try {
+    const { username, password } = req.body;
+    let userInDb = await User.findOne({ email: username });
+    if (userInDb) {
+      bcrypt.compare(password, userInDb.password, (err, isMatch) => {
+        if (isMatch) {
+          let tokenData = { userId: userInDb._id, username: userInDb.username };
+          const token = jwt.sign(tokenData, jwtSecret);
+
+          res.cookie("auth", token);
+
+          userInDb = { token, user: userInDb };
+
+          return res.render("dashboard", {
+            pageTitle: "Dashboard",
+            user: userInDb,
+          });
+          // console.log('o de bi')
+        } else {
+          // Incorrect password error
+          res.render("404", {
+            pageTitle: "Error",
+          });
+          console.log("incorrect pwd");
+        }
+      });
+    } else {
+      console.log("email not found eror");
+      res.render("404", {
+        pageTitle: "Error",
+      });
+    }
+  } catch (err) {
+    res.render("404", {
+      pageTitle: "Error",
+    });
+    console.log(err);
+  }
 });
 
+// shopRoutes.get("/dashboard", (req, res) => {
+//   res.render("dashboard", {
+//     pageTitle: "Dashboard",
+//   });
+// });
 //Signup middleware
 
 shopRoutes.get("/sign_up", (req, res) => {
@@ -25,45 +70,101 @@ shopRoutes.get("/sign_up", (req, res) => {
 //@route  POST
 //@desc   Register new User
 //@access Public
-shopRoutes.post("/register", (req, res) => {
-  console.log(req.body.email);
-  user
-    .findOne({ email: req.body.email })
-    .then((user) => {
-      if (user) {
-        res.status(404).json({ msg: "User Already Exist" });
-      } else {
-        const newUser = new User({
-          surname: req.body.surname,
-          firstname: req.body.firstname,
-          email: req.body.email,
-          password: req.body.password,
-          address: req.body.address,
-          username: req.body.username,
-          cardid: req.body.cardid,
-        });
+// shopRoutes.post("/register", (req, res) => {
+//   console.log(req.body.email);
+//   user
+//     .findOne({ email: req.body.email })
+//     .then((user) => {
+//       if (user) {
+//         res.status(404).json({ msg: "User Already Exist" });
+//       } else {
+//         const newUser = new User({
+//           surname: req.body.surname,
+//           firstname: req.body.firstname,
+//           email: req.body.email,
+//           password: req.body.password,
+//           address: req.body.address,
+//           username: req.body.username,
+//           cardid: req.body.cardid,
+//         });
 
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(newUser.password, salt, (err, hash) => {
-            newUser.password = hash;
+//         bcrypt.genSalt(10, (err, salt) => {
+//           bcrypt.hash(newUser.password, salt, (err, hash) => {
+//             newUser.password = hash;
 
-            newUser
-              .save()
-              .then((user) => res.redirect("/confirm"))
-              .catch((err) => console.log(err));
-            // Store hash in your password DB.
-          });
-        });
-      }
-    })
-    .catch((err) => {
-      res.json(err);
+//             newUser
+//               .save()
+//               .then((user) => res.redirect("/confirm"))
+//               .catch((err) => console.log(err));
+//             // Store hash in your password DB.
+//           });
+//         });
+//       }
+//     })
+//     .catch((err) => {
+//       res.json(err);
+//     });
+// });
+
+shopRoutes.post("/register", async (req, res) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res
+        .status(404)
+        .json({ msg: "User Already Exist" })
+        .redirect("/sign_up");
+    }
+
+    const newUser = new User({
+      surname: req.body.surname,
+      firstname: req.body.firstname,
+      email: req.body.email,
+      password: req.body.password,
+      address: req.body.address,
+      username: req.body.username,
+      cardid: req.body.cardid,
     });
+
+    await bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        newUser.password = hash;
+
+        newUser.save().then((user) => res.redirect("/confirm"));
+        // Store hash in your password DB.
+      });
+    });
+  } catch (err) {
+    res.render("404", {
+      pageTitle: "Error",
+    });
+    console.log(err);
+  }
 });
 
+//
+shopRoutes.get("/payment", (req, res) => {
+  res.render("payment", {
+    pageTitle: "Update Payment",
+  });
+});
+
+//
 shopRoutes.get("/confirm", (req, res) => {
   res.render("confirm", {
-    pageTitle: "Confirm your Account",
+    pageTitle: "Confirm Update",
+  });
+});
+
+shopRoutes.get("/addcustomer", (req, res) => {
+  res.render("addcustomer", {
+    pageTitle: "Add customer  ",
+  });
+});
+
+shopRoutes.get("/updatecustomer", (req, res) => {
+  res.render("updatecustomer", {
+    pageTitle: "Updat details ",
   });
 });
 
